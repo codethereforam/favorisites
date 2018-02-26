bac29a5335a4675e487ddaf7104e038dd82439be
codethereforam <codethereforam@gmail.com>
2018-02-26 20:21:28 +0800

    删除与登录、注册无关的文档

--- favorisites-web/src/main/webapp/resources/js/reset.js
+++ favorisites-web/src/main/webapp/resources/js/reset.js
@@ -1,3 +1,26 @@
-window.onload = function () {
+var accountNameValid = false;
+var emailCapthcaValid = false;
 
+function checkEmailSendButton() {
+
+}
+
+function checkNextStepButton() {
+
+}
+
+window.onload = function () {
+    var accountNameObject = document.getElementById("accountName");
+    var emailCaptchaObject = document.getElementById("emailCaptcha");
+
+    functions.checkAccountName = function () {
+
+    };
+
+    functions.checkEmailCaptcha = function () {
+
+    };
+
+    functions.checkAccountName();
+    functions.checkEmailCaptcha();
 };
